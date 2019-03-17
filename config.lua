da949290e5dd5d498d0c4a4f764a8c7a84e6a230
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-03-17 15:59:02 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,11 +2,11 @@
 Config = {}
 
 
-Config.lang = 'de' 
--- possible yet is en, de
-Config.noPirates = true --does kick players that has a cracked version of GTA.
+Config.lang = 'en' 
+-- possible yet is en, de, pl
+Config.noPirates = true --does kick players (before joining) that has a cracked version of GTA.
 
 Config.Mpos = 'left-top' --possible is 'left-top', 'right-top', 'center-top'
-Config.noglobalupdatemsg = false --Do not want to show a update global? change the value to true.
-Config.Zones = false --display your current location with this option.
-Config.NoHacks = true -- if enabled then players that are banned by Rockstar, will get kicked from your server.
+Config.noglobalupdatemsg = true --Do not want to show a update global? change the value to true.
+Config.Zones = true --display your current location with this option.
+Config.NoHacks = true -- if enabled then players that are banned by Rockstar, will get kicked (if joined) from your server.
